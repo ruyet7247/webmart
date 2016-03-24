e8bdf2f9ce4d3a260483f1c29ee5f8eb9488e496
ruyet7247 <ruyet7247@gmail.com>
2016-03-24 17:04:07 +0200

    Yönetim devam-2

--- Yonetim/KasaListesi.aspx
+++ Yonetim/KasaListesi.aspx
@@ -1,7 +1,157 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteYonetim.master" AutoEventWireup="true" CodeFile="KasaListesi.aspx.cs" Inherits="Yonetim_KasaListesi" %>
 
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit" tagprefix="asp" %>
+
+
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+    <style type="text/css">
+
+      
+        .style1
+        {
+            width: 120px;
+        }
+      
+        .style2
+        {
+            width: 147px;
+        }
+        .style3
+        {
+            width: 180px;
+        }
+        .style4
+        {
+            width: 36px;
+        }
+      
+     </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+ <asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
+    </asp:ToolkitScriptManager>
+        <table style="width:100%;">
+        <tr>
+            <td class="style2">
+                FİRMA
+                KASA RAPORLARI</td>
+            <td class="style3">
+                &nbsp;</td>
+            <td class="style4">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td class="style2">
+                Firma Seçimi Yapınız</td>
+            <td class="style3">
+                İlk Tarih -
+                Son Tarih</td>
+            <td class="style4">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td class="style2">
+                <asp:DropDownList ID="dd_firma" runat="server" 
+                    DataSourceID="SqlDataSource_firmalar" DataTextField="firma_adi" 
+                    DataValueField="connection_string_adi" Width="150px" 
+                    ondatabound="dd_firma_DataBound">
+                </asp:DropDownList>
+                <br />
+            </td>
+            <td class="style3">
+                    <asp:TextBox ID="txt_bas_tarih" runat="server" Width="80px"></asp:TextBox>
+                    <asp:CalendarExtender ID="txt_bas_tarih_CalendarExtender" runat="server" 
+                        Enabled="True" Format="dd.MM.yyyy" TargetControlID="txt_bas_tarih">
+                    </asp:CalendarExtender>
+                    <asp:TextBox ID="txt_son_tarih" runat="server" Width="80px"></asp:TextBox>
+                    <asp:CalendarExtender ID="txt_son_tarih_CalendarExtender" runat="server" 
+                        Enabled="True" Format="dd.MM.yyyy" TargetControlID="txt_son_tarih">
+                    </asp:CalendarExtender>
+                   
+                    </td>
+            <td class="style4">
+                &nbsp;</td>
+            <td class="style1">
+                <asp:ImageButton ID="ibtn_raporla" runat="server" onclick="ibtn_raporla_Click" 
+                    CssClass="GridViewClass" ImageUrl="~/Icons/res3232/arama2.png" />
+            </td>
+            <td class="style1">
+                <asp:SqlDataSource ID="SqlDataSource_firmalar" runat="server" 
+                    ConnectionString="<%$ ConnectionStrings:WebMart_Master %>" 
+                    SelectCommand="SELECT [firma_adi], [connection_string_adi] FROM [firma_kayit]">
+                </asp:SqlDataSource>
+            </td>
+            <td class="style1">
+                &nbsp;</td>
+        </tr>
+          <tr>
+            <td class="style1">
+                Toplam Giriş</td>
+            <td class="style1">
+                    <asp:TextBox ID="txt_giren" runat="server" CssClass="sagaDayaliFormat" 
+                        ReadOnly="True" Width="120px"></asp:TextBox>
+                    </td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td class="style1">
+                Toplam Çıkış</td>
+            <td class="style1">
+                    <asp:TextBox ID="txt_cikan" runat="server" CssClass="sagaDayaliFormat" 
+                        ReadOnly="True" Width="120px"></asp:TextBox>
+                    </td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td class="style1">
+                Bakiye</td>
+            <td class="style1">
+                    <asp:TextBox ID="txt_bakiye" runat="server" CssClass="sagaDayaliFormat" 
+                        ReadOnly="True" Width="120px"></asp:TextBox>
+                    </td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+            <td class="style1">
+                &nbsp;</td>
+        </tr>
+        </table>
+        <asp:Panel ID="pnl_listele" runat="server">
+            <asp:Table ID="Table1" runat="server" BorderColor="#000099" BorderStyle="Solid" 
+                BorderWidth="1px" GridLines="Both">
+            </asp:Table>
+    </asp:Panel>
+    <p>
+        <asp:Label ID="lbl_mesaj" runat="server"></asp:Label>
+    </p>
 </asp:Content>
 
--- Yonetim/KasaListesi.aspx.cs
+++ Yonetim/KasaListesi.aspx.cs
@@ -4,11 +4,198 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data; // veritabanı
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Threading;
 
 public partial class Yonetim_KasaListesi : System.Web.UI.Page
 {
+    
     protected void Page_Load(object sender, EventArgs e)
     {
+        lbl_mesaj.Text = "";
+        SqlDataSource_firmalar.ConnectionString = WebConfigurationManager.ConnectionStrings[Session["ConnectionString"].ToString()].ConnectionString;
+
+        if (!IsPostBack)  // tıklama ile sayfa gelmemiş ise
+        {
+            DateTime.Now.Year.ToString();
+            DateTime date = DateTime.Today;
+
+            txt_bas_tarih.Text = "01-01-" + DateTime.Now.Year.ToString();
+            txt_son_tarih.Text = date.ToString("dd-MM-yyyy");
+        }
+
+       
 
     }
+
+    protected void ibtn_raporla_Click(object sender, ImageClickEventArgs e)
+    {
+        TabloAyarla();
+        if (dd_firma.SelectedValue == "tum") { return; }
+
+        //KasaHareketListesiniGetir(dd_firma.SelectedValue);
+        //KasaHareketToplamlariGetir(dd_firma.SelectedValue);
+        //string queryStringGiren = "SELECT TOP (100) PERCENT dbo.kasa_kayit.kasa_id, dbo.kasa_kayit.kasa_adi + ' - ' + dbo.firma_para_birimi_tanimlama.para_birimi AS kasa, dbo.kasa_kayit.kasa_adi, dbo.kasa_kayit.para_birimi_id, dbo.kasa_kayit.aciklama1, dbo.firma_para_birimi_tanimlama.para_birimi FROM dbo.kasa_kayit INNER JOIN dbo.firma_para_birimi_tanimlama ON dbo.kasa_kayit.para_birimi_id = dbo.firma_para_birimi_tanimlama.para_birimi_id ORDER BY dbo.kasa_kayit.kasa_adi";
+        
+        string queryStringGiren = "SELECT TOP (100) PERCENT dbo.kasa_kayit.kasa_id, dbo.kasa_kayit.kasa_adi + ' - ' + dbo.firma_para_birimi_tanimlama.para_birimi AS kasa, dbo.kasa_kayit.kasa_adi, dbo.kasa_kayit.para_birimi_id, dbo.kasa_kayit.aciklama1, dbo.firma_para_birimi_tanimlama.para_birimi FROM dbo.kasa_kayit INNER JOIN dbo.firma_para_birimi_tanimlama ON dbo.kasa_kayit.para_birimi_id = dbo.firma_para_birimi_tanimlama.para_birimi_id ORDER BY dbo.kasa_kayit.kasa_adi";
+        ConnVt baglan = new ConnVt(); SqlConnection connection = baglan.VeritabaninaBaglan(dd_firma.SelectedValue); SqlCommand cmd = new SqlCommand(queryStringGiren, connection);
+        try
+        {
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    KasaHareketToplamlariGetir(dd_firma.SelectedItem.Text,dd_firma.SelectedValue,reader["kasa_id"].ToString());
+                    
+                }
+            }
+        }
+        catch (Exception err)
+        {
+            lbl_mesaj.Text += "Error Kasa İsimleri Çağırma. ";
+            lbl_mesaj.Text += err.Message;
+        }
+        finally
+        {
+            baglan.VeritabaniBaglantiyiKapat(connection);
+        }
+
+    }
+
+    protected void TabloAyarla() {
+        // Total number of rows.
+        int rowCnt;
+        // Current row count.
+        int rowCtr;
+        // Total number of cells per row (columns).
+        int cellCtr;
+        // Current cell counter
+        int cellCnt;
+
+        // Create new row and add it to the table.
+            TableRow tRow = new TableRow();
+            Table1.Rows.Add(tRow); TableCell tCell = new TableCell(); tCell.Text = "Kasa Adı "; tCell.Width = 200; tRow.Cells.Add(tCell);
+            Table1.Rows.Add(tRow); tCell = new TableCell(); tCell.Text = "Giriş Bakiye "; tCell.Width = 200; tRow.Cells.Add(tCell);
+            Table1.Rows.Add(tRow); tCell = new TableCell(); tCell.Text = "Çıkış Bakiye "; tCell.Width = 200; tRow.Cells.Add(tCell);
+            Table1.Rows.Add(tRow); tCell = new TableCell(); tCell.Text = "Bakiye "; tCell.Width = 200; tRow.Cells.Add(tCell);
+
+        /*
+        rowCnt = 1;
+        cellCnt = 3;
+        for (rowCtr = 1; rowCtr <= rowCnt; rowCtr++)
+        {
+            // Create new row and add it to the table.
+            TableRow tRow = new TableRow();
+            Table1.Rows.Add(tRow);
+            for (cellCtr = 1; cellCtr <= cellCnt; cellCtr++)
+            {
+                // Create a new cell and add it to the row.
+                TableCell tCell = new TableCell();
+                tCell.Text = "Kasa Adı " + rowCtr + ", Kasa " + cellCtr;
+                tRow.Cells.Add(tCell);
+            }
+        }
+        */
+    }
+
+   
+    protected void KasaHareketToplamlariGetir(string kasa_adi,string ConnStr,string kasa_id)
+    {
+
+        DateTime ilktarih = Convert.ToDateTime(txt_bas_tarih.Text);
+        DateTime sontarih = Convert.ToDateTime(txt_son_tarih.Text);
+        double giren_toplam = 0;
+        double cikan_toplam = 0;
+        double bakiye = 0;
+        txt_giren.Text = "0"; txt_cikan.Text = "0";
+
+        TableRow tRow = new TableRow();
+        TableCell tCell;
+        Table1.Rows.Add(tRow); tCell = new TableCell(); tCell.Text = kasa_adi + " "; tCell.Width = 200; tRow.Cells.Add(tCell);
+
+        string queryStringGiren = "SELECT sum(tutar) AS giren FROM kasa_hareket WHERE giris_or_cikis='giris' and kasa_id='" + kasa_id + "' and (kayit_tarihi BETWEEN '" + ilktarih + "' and '" + sontarih + "')";
+        ConnVt baglan = new ConnVt(); SqlConnection connection = baglan.VeritabaninaBaglan(ConnStr); SqlCommand cmd = new SqlCommand(queryStringGiren, connection);
+        try
+        {
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                        txt_giren.Text = reader["giren"].ToString();
+                        giren_toplam = Convert.ToDouble(reader["giren"].ToString());
+                        Table1.Rows.Add(tRow); tCell = new TableCell(); tCell.Text = String.Format("{0:#,#.00}", giren_toplam) + " "; tCell.Width = 200; tRow.Cells.Add(tCell);
+       
+                }
+              
+            }
+            
+        }
+        catch (Exception err)
+        {
+            lbl_mesaj.Text += "Error Kasa Hesaplama Giriş. ";
+            lbl_mesaj.Text += err.Message;
+        }
+        finally
+        {
+            baglan.VeritabaniBaglantiyiKapat(connection);
+        }
+
+        string queryStringCikan = "SELECT sum(tutar) AS cikan FROM kasa_hareket WHERE giris_or_cikis='cikis' and kasa_id='" + kasa_id + "' and (kayit_tarihi BETWEEN '" + ilktarih + "' and '" + sontarih + "')";
+        ConnVt baglan2 = new ConnVt(); SqlConnection connection2 = baglan2.VeritabaninaBaglan(ConnStr); SqlCommand cmd2 = new SqlCommand(queryStringCikan, connection2);
+        try
+        {
+            SqlDataReader reader2 = cmd2.ExecuteReader();
+            if (reader2.HasRows)
+            {
+                while (reader2.Read())
+                {
+                        txt_cikan.Text = reader2["cikan"].ToString();
+                        cikan_toplam = Convert.ToDouble(reader2["cikan"].ToString());
+                        Table1.Rows.Add(tRow); tCell = new TableCell(); tCell.Text = String.Format("{0:#,#.00}", cikan_toplam) + " "; tCell.Width = 200; tRow.Cells.Add(tCell);
+                }
+
+            }
+            
+        }
+        catch (Exception err)
+        {
+            lbl_mesaj.Text += "Error Kasa Hesaplama Çıkış. ";
+            lbl_mesaj.Text += err.Message;
+        }
+        finally
+        {
+            baglan2.VeritabaniBaglantiyiKapat(connection2);
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        try
+        {
+            bakiye = giren_toplam - cikan_toplam;
+            txt_bakiye.Text = String.Format("{0:#,#.00}", bakiye);
+            Table1.Rows.Add(tRow);  tCell = new TableCell(); tCell.Text = String.Format("{0:#,#.00}", bakiye)+" "; tCell.Width = 200; tRow.Cells.Add(tCell);
+            
+            
+        }
+        catch (Exception err)
+        {
+            lbl_mesaj.Text += "Error Kasa GirisCikisToplaminiGetir. ";
+            lbl_mesaj.Text += err.Message;
+        }
+
+
+
+    }
+    
+
+    protected void dd_firma_DataBound(object sender, EventArgs e)
+    {
+        dd_firma.Items.Insert(0, new ListItem("- Firma Seçiniz -", "tum"));
+    }
+
+
+
 }
